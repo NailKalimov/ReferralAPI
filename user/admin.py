dd803789507d77bbf9fc35c5637cb443c807a44f
Nail Kalimov <nailkalimov@gmail.com>
2025-03-10 13:56:27 +0400

    Update user model

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+from django.utils.translation import gettext_lazy as _
+
 
 class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'referrer')
@@ -9,9 +11,26 @@ class CustomUserAdmin(UserAdmin):
             None,
             {
                 'classes': ('wide',),
-                'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'referrer'),
+                'fields': ('email', 'username', 'password1', 'password2', 'referrer'),
             },
         ),
+    )
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "referrer")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
 
 
--- user/models.py
+++ user/models.py
@@ -37,7 +37,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractUser):
     referrer = models.ForeignKey('User', verbose_name="Реферер", blank=True, null=True, on_delete=models.SET_NULL)
-    objects = UserManager()
+    # objects = UserManager()
 
     def __str__(self):
         return self.username
